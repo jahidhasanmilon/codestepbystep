fad2ca2012f9f8a3f821a31866ecb18f0f1fb1f1
Jahid Hasan Milon <81589991+jahidhasanmilon@users.noreply.github.com>
2021-04-22 01:51:17 +0600

    Create fixErrors.cpp

---
+++ HomeWork1/fixErrors.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+    
+    int age=0;
+    cout << "How old are you? ";
+    cin >> age;
+    
+    cout << "You are "<<age<<", wow!"<<endl;
+}
