5de5164c80579f2f490a0462a9244c665702194f
Jahid Hasan Milon <81589991+jahidhasanmilon@users.noreply.github.com>
2021-04-22 02:13:58 +0600

    Create  percentageGrade.cpp

---
+++ HomeWork1/percentageGrade.cpp
@@ -0,0 +1,23 @@
+int percent;
+cout << "What percentage did you earn? ";
+cin >> percent;
+if (percent >= 90)
+{
+    cout << "You got an A!" << endl;
+}
+else if (percent >= 80)
+{
+    cout << "You got a B!" << endl;
+}
+else if (percent >= 70)
+{
+    cout << "You got a C!" << endl;
+}
+else if (percent >= 60)
+{
+    cout << "You got a D!" << endl;
+}
+else
+{
+    cout << "You got an F!" << endl;
+}
